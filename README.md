853ee0361fc6115649fbd3f375a76e2f62127e0c
Hunter Gruszczynski <Hgruszcz@gmail.com>
2020-09-24 17:34:59 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# Budget-tracker
+# Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Questions](#questions)
+
+## Description:
+
+A PWA application that tracks a user's budget whether online or offline using mongoose and express.
+
+## Installation:
+npm install, mongoDB
+
+## Usage:
+npm run start
+
+## License:
+
+For more info about the license, follow the link below.
+
+[unlicense](https://opensource.org/licenses/unlicense)
+
+## Contributing:
+Pull requests 
+
+## Questions?:
+
+Contact me at: hgruszcz@gmail.com for questions.
---
+++ public/service-worker.js
@@ -0,0 +1,54 @@
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png',
+  '/manifest.webmanifest',
+];
+
+const CACHE_NAME = 'static-cache-v1';
+const DATA_CACHE_NAME = 'data-cache-v1';
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(FILES_TO_CACHE))
+      .then(self.skipWaiting()),
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => Promise.all(
+      cacheNames.map((cache) => {
+        if (cache !== CACHE_NAME && cache !== DATA_CACHE_NAME) {
+          console.log('Removing chace data', cache);
+          return caches.delete(cache);
+        }
+      }),
+    )),
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.url.includes('/api/')) {
+    event.respondWith(
+      caches.open(DATA_CACHE_NAME).then((cache) => fetch(event.request)
+        .then((response) => {
+          if (response.status === 200) {
+            cache.put(event.request.url, response.clone());
+          }
+
+          return response;
+        })
+        .catch(() => cache.match(event.request)))
+        .catch((err) => console.log(err)),
+    );
+    return;
+  }
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request)),
+  );
+});
